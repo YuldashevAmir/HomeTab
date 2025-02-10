db7fc6d2a40c32037ac80a7e05d4e88df2a7cfd6
Amir <amirYuld@gmail.com>
2025-02-10 16:17:26 +0500

    [FIX] Timetable

--- src/static/TimeTable.ts
+++ src/static/TimeTable.ts
@@ -9,14 +9,28 @@ type TLecture = {
 }
 
 export const timeTable: ITimeTable = {
-	0: [
-		{ title: 'CN', room: 'B210', time: '9:30' },
-		{ title: 'CN', room: 'B210', time: '9:30' },
+	0: [],
+	1: [
+		{ title: 'SaS', room: 'B202', time: '9:30' },
+		{ title: 'ItBA', room: 'A203', time: '11:00' },
+		{ title: 'DA&D', room: 'A203', time: '12:30' },
+		{ title: 'CN', room: 'A203', time: '15:30' },
 	],
-	1: [{ title: 'CN', room: 'B210', time: '11:00' }],
-	2: [{ title: 'CN', room: 'B210', time: '12:30' }],
-	3: [{ title: 'CN', room: 'B210', time: '14:00' }],
-	4: [{ title: 'CN', room: 'B210', time: '15:30' }],
+	2: [
+		{ title: 'SaS', room: 'B202', time: '11:00' },
+		{ title: 'P&S', room: 'B209', time: '14:00' },
+		{ title: 'UP', room: 'B209', time: '15:30' },
+		{ title: 'UAH', room: 'B304', time: '17:00' },
+	],
+	3: [
+		{ title: 'ItBA', room: 'A605', time: '11:00' },
+		{ title: 'DA&D', room: 'B201', time: '14:00' },
+		{ title: 'UP', room: 'B201', time: '15:30' },
+	],
+	4: [
+		{ title: 'P&S', room: 'B210', time: '9:30' },
+		{ title: 'CN', room: 'B209', time: '11:00' },
+	],
 	5: [],
 	6: [],
 }
